e49d44af2bce5d5025f199741a45ea9cafd6b728
Alessandro <alessandronobre.ti@gmail.com>
2025-02-15 17:03:38 -0300

    Subindo projeto

---
+++ src/main/java/br/com/example/nutritional_assistant_api/NutritionalAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.nutritional_assistant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionalAssistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionalAssistantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/nutritional_assistant_api/NutritionalAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.nutritional_assistant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionalAssistantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
